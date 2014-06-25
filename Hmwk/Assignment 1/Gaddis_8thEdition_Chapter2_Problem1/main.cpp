1aeb455fce8931b448d0784e6746a0b325f117d7
Implication <Newfiremoy@gmail.com>
2014-06-25 11:10:08 -0700

    Signed-off-by: Implication <Newfiremoy@gmail.com>

---
+++ Hmwk/Assignment 1/Gaddis_8thEdition_Chapter2_Problem1/main.cpp
@@ -0,0 +1,31 @@
+/* 
+* File:   main.cpp
+* Author: Trajon Felton
+*
+* Created on June 24, 2014, 12:04 PM
+* Purpose: Homework Problem 1 Chapter 2
+*/
+
+//System File Libraries
+#include <iostream>
+using namespace std;
+
+//User Define Libraries
+
+//Global constants
+
+//Function Prototypes
+
+
+//Execution begins here
+int main(int argc, char** argv) {
+   //Declared variables
+   short x = 62, y = 99; // Stores the integer 62 in the variable x, and 99 in the variable y
+   //Calculations
+   short total = x + y; // The sum of the variables x and y
+   //Display output on screen
+   cout << "The sum of 62 and 99 is equal to " << total;
+   //Exit stage right
+   return 0;
+}
+
