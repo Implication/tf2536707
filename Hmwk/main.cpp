a918337fd73f63d2e405e1f8dc94c26aefc20093
Trajon Felton <Newfiremoy@gmail.com>
2014-06-25 07:43:52 -0700

    Hello World

---
+++ Hmwk/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Newfiremoy
+ *
+ * Created on June 23, 2014, 8:41 PM
+ * Purpose: First Program
+ */
+
+//System Level Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Libraries
+
+//Function Prototypes
+
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Output Simple Text
+    cout << "Hello World" << endl;
+    //Exit stage right
+    return 0;
+}
+
