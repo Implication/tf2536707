ae104f2950ff42473acd60d17359673d1b6cda70
Implication <Newfiremoy@gmail.com>
2014-07-01 12:40:32 -0700

    Signed-off-by: Implication <Newfiremoy@gmail.com>

--- Hmwk/Assignment 2/Gaddis_8thEdition_Chapter3_Problem8/main.cpp
+++ Hmwk/Assignment 2/Gaddis_8thEdition_Chapter3_Problem8/main.cpp
@@ -21,33 +21,35 @@ using namespace std;
 int main(int argc, char** argv) {
 //Declare Variables
     string name; //Name of the movie
-    float total, profit, paid; //Total amount of money made from the movie, the profit the movie theature makes, the amount paid to the distrubutor
+    float total, profit, paid; //Total amount of money made from the movie, the profit the movie theaters makes, the amount paid to the distributor
     unsigned short cTick, aTick; //Number of tickets sold for children, Number of tickets sold for adults
 //Prompt user
     cout << "Enter the name of the movie:" << endl;
-    getline(cin, name);
-    cout << "Enter the number of tickets sold for children:" << endl;
-    cin >> cTick;
+    getline(cin, name); //Gets the name of the movie
     cout << "Enter the number of tickets sold for adults:" << endl;
-    cin >> aTick;
+    cin >> aTick; //Gets the number of tickets sold for adults
+    cout << "Enter the number of tickets sold for children:" << endl;
+    cin >> cTick; //Gets the number of tickets sold for children
+    cout << sizeof(name);
 //Calculations
-    total = (cTick * 3) + (aTick * 6);
-    profit = total * .2;
-    paid = total * .8;
+    total = (cTick * 3) + (aTick * 6); //Calculates the total amount of gross pay, children tickets are 3 dollars, and adult tickets are 6 dollars
+    profit = total * .2; //Takes the total, and multiples it by .2 to get 20% of the total amount for the Net box
+    paid = total * .8; //Takes the total, and multiples it by .8 to get 80% of the toal amount for the distributor
 //Display output
     cout << endl << setprecision(2) << fixed << showpoint;
     cout << left << setw(24) << "Movie Name:" <<
-            right << setw(12) << "\"" << name << "\"" << endl;
+            right << setw(10) << "\"" << name << "\"" << endl;
     cout << left << setw(24) << "Adult Tickets Sold:" << 
-            right << setw(19) << aTick << endl;
+            right << setw(14) << aTick << endl;
     cout << left << setw(24) << "Child Tickets Sold:" << 
-            right << setw(19) << cTick << endl;
+            right << setw(14) << cTick << endl;
     cout << left << setw(24) << "Gross Box Office Profit:" <<
-            right << setw(16) << "$" << total << endl;
+            right << setw(12) << "$" << right << setw(8) << total << endl;
     cout << left << setw(24) << "Amount Paid to Distributor:" <<
-            right << setw(13) << "$" << paid << endl;
+            right << setw(9) << "-$" <<  right << setw(8) << paid << endl;
     cout << left << setw(24) << "Net Box Office Profit:" <<
-            right << setw(16) << "$" << profit << endl;
+            right << setw(12) << "$" <<  right << setw(8) << profit << endl;
+ //Exit Stage Right
     return 0;
 }
 
