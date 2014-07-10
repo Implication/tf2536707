e5b5a634027128e8aed30328a0ba8439d655f37a
Implication <Newfiremoy@gmail.com>
2014-07-10 13:37:02 -0700

    Signed-off-by: Implication <Newfiremoy@gmail.com>

---
+++ Project 1/Project/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: Trajon Fetlon
+ * Created on July 10, 2014, 1:31 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
